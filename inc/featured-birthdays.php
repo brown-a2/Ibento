17c0c3a308322e3e431828011a87ad6fd2030fd3
adam <brown-a2@users.noreply.github.com>
2024-07-31 23:27:23 +0100

    Fix post birthday order and styling

--- inc/featured-birthdays.php
+++ inc/featured-birthdays.php
@@ -5,7 +5,7 @@
  * Calculate the age on the date of the given birthday date.
  *
  * @param string $birthday_date The birthday date in 'YYYYMMDD' format.
- * @return string The age in a string format "X years, Y months, Z days".
+ * @return string The age in a string format "X years".
  */
 function ibento_calculate_age_on_birthday($birthday_date) {
     // Create DateTime objects for the birthday date
@@ -15,28 +15,39 @@ function ibento_calculate_age_on_birthday($birthday_date) {
         return 'Invalid date'; // Return an error message if the date format is incorrect
     }
 
-    // Get today's date and create a DateTime object for the next birthday
+    // Get today's date
     $today = new DateTime();
+
+    // Calculate the age at the end of this year
     $current_year_birthday = new DateTime($today->format('Y') . $birthday->format('-m-d'));
 
     // If the birthday has already occurred this year, calculate for the next year
     if ($current_year_birthday < $today) {
-        $current_year_birthday->modify('+1 year');
+        $age_on_birthday = $today->format('Y') - $birthday->format('Y') + 1;
+    } else {
+        $age_on_birthday = $today->format('Y') - $birthday->format('Y');
     }
 
-    // Calculate the age
-    $age = $current_year_birthday->diff($birthday);
+    if ($current_year_birthday == $today) {
+        return $age_on_birthday . ' years old today! &#127880;';
+    }
 
     // Format the age as a string
-    return $age->y . ' years, ' . $age->m . ' months, ' . $age->d . ' days';
+    return $age_on_birthday . ' years old';
 }
 
+/**
+ * Convert date format from 'YYYYMMDD' to 'F j, Y'.
+ *
+ * @param string $postDate The date in 'YYYYMMDD' format.
+ * @return string The formatted date.
+ */
 function ibentoConvertDate($postDate) {
-    // Check if the date is set in the $_POST array
+    // Check if the date is set
     if (isset($postDate)) {
         // Create a DateTime object from the input date
         $date = DateTime::createFromFormat('Ymd', $postDate);
-;        
+        
         // Check if the date is valid
         if ($date) {
             // Format the date to 'F j, Y'
@@ -50,19 +61,18 @@ function ibentoConvertDate($postDate) {
 }
 
 
-// Render callback function for the dynamic block
-function ibento_birthday_block_render_callback($attributes) {
-
+function ibento_birthday_block_render_callback() {
     // Get today's date and current day-month
-    $today = date('Ymd'); // Format today's date in 'YYYYMMDD'
+    $posts_to_show = 6;
+
     $today_day_month = date('md'); // Format today's day and month as 'MMDD'
 
     $args = array(
         'post_type' => 'birthday',
-        'posts_per_page' => 5, // Retrieve all posts for custom sorting
+        'posts_per_page' => -1, // Retrieve all posts for custom sorting
         'meta_key' => 'birthday_date',
         'orderby' => 'meta_value',
-        'order' => 'ASC', // Order ascending to get nearest future dates
+        'order' => 'DEC', // Order ascending to get nearest future dates
         'meta_type' => 'DATE'
     );
 
@@ -79,6 +89,7 @@ function ibento_birthday_block_render_callback($attributes) {
                 $birthday_day_month = date('md', strtotime($birthday_date));
                 // Include all dates for the rolling year
                 $birthday_posts[] = [
+                    'ID' => get_the_ID(),
                     'title' => get_the_title(),
                     'date' => $birthday_date,
                     'day_month' => $birthday_day_month,
@@ -108,7 +119,7 @@ function ibento_birthday_block_render_callback($attributes) {
         $birthday_posts = array_merge($current_year_posts, $next_year_posts);
 
         // Limit to the nearest 10 birthdays
-        $birthday_posts = array_slice($birthday_posts, 0, 10);
+        $birthday_posts = array_slice($birthday_posts, 0, $posts_to_show);
 
         // Generate output
         if (!empty($birthday_posts)) {
@@ -116,11 +127,10 @@ function ibento_birthday_block_render_callback($attributes) {
             foreach ($birthday_posts as $post) {
                 $age_on_birthday = ibento_calculate_age_on_birthday($post['date']);
                 $output .= '<li class="birthday-item">';
-                $output .= '<span class="birthday-title">' . esc_html($post['title']) . '</span> ';
-                $output .= '<span class="birthday-date">' . esc_html(ibentoConvertDate($post['date'])) . '</span> ';
-                $output .= '<span class="birthday-age">| ' . esc_html($age_on_birthday) . '</span>';
+                $output .= '<span class="birthday-title"><a href="' . get_permalink($post['ID']) . '">' . esc_html($post['title']) . '</a></span> ';
+                $output .= '<span class="birthday-age"> Will be turning <span class="birthday-date-number">' . esc_html($age_on_birthday) . '</span></span>';
+                $output .= '<span class="birthday-date"> | <span class="birthday-date-title">Birthday</span>:<br> | ' . esc_html(ibentoConvertDate($post['date'])) . '</span> ';
                 $output .= '</li>';
-                
             }
             $output .= '</ul>';
         } else {
@@ -134,6 +144,8 @@ function ibento_birthday_block_render_callback($attributes) {
 
     return $output;
 }
+
+
 
 // Register the dynamic block
 function ibento_register_birthday_block() {
